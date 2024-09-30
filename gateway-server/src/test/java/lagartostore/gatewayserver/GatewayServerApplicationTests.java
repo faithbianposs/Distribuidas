349b6055c8f11b07bb8862724c8cd34219e2c0ad
faithbianposs <ramosfrank318@gmail.com>
2024-09-29 22:16:38 -0500

    gateway

---
+++ gateway-server/src/test/java/lagartostore/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lagartostore.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
